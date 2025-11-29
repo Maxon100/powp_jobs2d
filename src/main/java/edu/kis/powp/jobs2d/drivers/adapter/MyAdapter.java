7a64dbb933411d111a49efcd5251ed9f1b3e0980
Maxon100 <maks.swiatczak@gmail.com>
2025-11-29 11:38:04 +0100

    Wyswietla kwadrat

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -26,6 +26,11 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+		
+		// line.drawline(line)
+
+		this.startX = x;
+		this.startY = y;	
 
 		drawLine(line);
 	}
